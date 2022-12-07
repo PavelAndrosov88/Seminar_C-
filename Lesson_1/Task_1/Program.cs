611aef9d273a50c141c885157df52e02c40f81fb
Pavel <andros88pa@mail.ru>
2022-12-07 21:47:09 +0700

    Lesson_1

---
+++ Lesson_1/Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Clear();
+Console.Write("Введите 1-oe число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-oe число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine($"max = {a} min = {b}");
+else
+    Console.WriteLine($"max = {b} min = {a}");
---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-oe число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-e число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
