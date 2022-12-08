adde73ed26da608197ccbb3f14a6615745672324
Pavel <andros88pa@mail.ru>
2022-12-08 20:13:45 +0700

    Lesson_3

---
+++ Lesson_3/Task_19/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+string? s = Console.ReadLine();
+
+if(s?[0] == s?[4] && s?[1] == s?[3])
+    Console.WriteLine("ДА");
+else 
+    Console.WriteLine("НЕТ");
