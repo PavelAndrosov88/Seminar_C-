1993a66b08d16965e94c349e30919fd8a980337a
Pavel <andros88pa@mail.ru>
2022-12-20 19:35:17 +0700

    Lesson_6

---
+++ Lesson_6/Task_41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write ("Введите числа через пробел: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int sum = 0;
+
+foreach (int element in size)
+    if (element > 0)
+    sum ++;
+Console.WriteLine($"Колличетво чисел больше 0 = {sum} ");
